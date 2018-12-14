6b7486907528a6d6069307468fdbb9718b0ecfcf
Daniel Hidalgo G <daniel.hg94@gmail.com>
2018-12-13 22:56:39 -0300

    Added logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :histories
-
+  get 'users/admin'
+  post 'users/admin/:id', to: 'users#def_admin', as: 'def_admin'
+  devise_for :users, controllers: {
+   registrations: 'users/registrations',
+   passwords: 'users/passwords',
+   sessions: 'users/sessions'
+  }
+  get 'histories/mines'
+  resources :histories
   root "histories#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
